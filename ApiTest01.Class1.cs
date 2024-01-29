33d809334a23ef17b9d71ceaf0017f975fd77e60
JavaCommons Technologies <javacommons@gmail.com>
2024-01-29 12:48:05 +0900

    .

---
+++ ApiTest01.Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace ApiTest01
+{
+    public class Class1
+    {
+        [DllExport]
+        public static int add2(int a, int b)
+        {
+            return a + b;
+        }
+        static ThreadLocal<IntPtr> helloPtr = new ThreadLocal<IntPtr> ();
+        [DllExport]
+        public static IntPtr hello()
+        {
+            if (helloPtr.Value != IntPtr.Zero)
+            {
+                Console.WriteLine("Freeing helloPtr...");
+                Marshal.FreeHGlobal(helloPtr.Value);
+                helloPtr.Value = IntPtr.Zero;
+            }
+            string x = "hello ハロー©";
+            helloPtr.Value = Marshal.StringToHGlobalUni(x);
+            return helloPtr.Value;
+        }
+        [DllExport]
+        public static void puts(IntPtr ws)
+        {
+
+
+            var s = Marshal.PtrToStringUni(ws);
+            Console.WriteLine($"Hello {s}");
+        }
+    }
+}
